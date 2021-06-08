5c8994440a7cea97c2a152213cd12b68ce1ff4bd
kataixxv <80951271+tokioxxv@users.noreply.github.com>
2021-06-08 15:57:38 +0500

    bug is fixed
    wont float on the ground now

--- eragon/Assets/Scripts/ThirdPersonMovement.cs
+++ eragon/Assets/Scripts/ThirdPersonMovement.cs
@@ -7,26 +7,27 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 300f;
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        Vector3 projectedCameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
+        Quaternion rotationToCamera = Quaternion.LookRotation(projectedCameraForward, Vector3.up);
+
+        moveDirection = rotationToCamera * moveDirection;
+        Quaternion rotationToMove = Quaternion.LookRotation(moveDirection, Vector3.up);
+
+        if (moveDirection.magnitude > 0)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * Time.deltaTime * speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMove, rotationSpeed * Time.deltaTime);
         }
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
     }
 
 }
